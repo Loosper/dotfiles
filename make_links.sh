df968948dca2fb061d77c7adc0c2121bd619a971
Boyan Karatotev <boyan.karatotev@arm.com>
2021-10-17 14:40:23 +0100

    make links for htoprc and the .zsh_system_source

--- make_links.sh
+++ make_links.sh
@@ -1,4 +1,6 @@
 mkdir -p ~/.vim/colors
+mkdir -p ~/.config/htop
+mkdir -p ~/system
 
 ln -s `pwd`/.gdbinit ~/.gdbinit
 ln -s `pwd`/.gitconfig ~/.gitconfig
@@ -6,6 +8,7 @@ ln -s `pwd`/.pythonrc ~/.pythonrc
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.zsh_system_source ~/system/.zsh_system_source
 ln -s `pwd`/config ~/.ssh/config
 ln -s `pwd`/.gitignore ~/.gitignore
 ln -s `pwd`/badwolf.vim ~/.vim/colors/badwolf.vim
